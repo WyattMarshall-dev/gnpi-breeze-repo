d29753ad12d005644b61a2a28adaf75a6b0bd6f4
WyattMarshall-dev <wyatt_marshall@hotmail.com>
2021-10-12 02:09:16 -0700

    fuck this

--- config/filesystems.php
+++ config/filesystems.php
@@ -67,7 +67,7 @@ return [
     */
 
     'links' => [
-        public_path('storage') => storage_path('app/public'),
+        public_path('storage/thumbnails') => storage_path('app/public/thumbnails'),
     ],
 
 ];
